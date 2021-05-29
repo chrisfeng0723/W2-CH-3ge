0528a0630f4f2f0a4efbb66b0c7bf63d20dd9799
fengxinlei <fengxinlei@360.cn>
2021-05-29 22:21:56 +0800

    增加常用函数

--- utils/functions.go
+++ utils/functions.go
@@ -7,7 +7,11 @@
 
 package utils
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+	"unicode"
+)
 
 //根据一个文件名字符串获取其中的数字
 //eg:W-2_1.gjf.gjf.gjf.log
@@ -18,3 +22,26 @@ func GetFileNumber(fileName string) string {
 	params := Regexp.FindStringSubmatch(fileName)
 	return params[1]
 }
+
+
+//获取文件的HF值
+func GetFileHF(fileContent string) string {
+
+	//str := `HF=(-?\d+.\d+)\\`
+	str := `HF=(-?\d+.\d+)\\`
+	Regexp := regexp.MustCompile(str)
+	//去除空白字符
+	temp := strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, fileContent)
+	params := Regexp.FindStringSubmatch(temp)
+	if len(params) > 0 {
+		return params[1]
+	}
+	return ""
+}
+
+
